bc32b8d05cdc9b181ccc7f6939cbe62fc12249d4
sumfaul-vic <75687255+sumfaul-vic@users.noreply.github.com>
2021-05-26 21:19:16 +0100

    HTML/CSS:button and style jQuery: added game play functions Todo: pull gameplay together

--- Simon_Game/index.js
+++ Simon_Game/index.js
@@ -1,33 +1,77 @@
+// event listeners
 $("button").click(function(event) {
-console.log(event);
+  if (simonSays.length === 0) {
+    $(".title").text("Press start to play");
+  } else {
+    let x = $(this)
+    let btn = this.classList[1]
 
-  var x = $(this)
-  var newClass = this.classList[1] + "-press";
-  animationJump(x,newClass);
+    let newClass = createPressAnimation(btn)
+    let btnSound = createSoundFileName(btn);
+
+    animationJump(x, newClass, btnSound);
+    if (compareWithSimon(btn)) {
+      console.log("Well done");
+      userInput++;
+    } else {
+      loserReset();
+    }
+  }
+
 
 });
 
 $("button").on("touchstart", function(event) {
+
   var x = $(this)
-  var newClass = this.classList[1] + "-press";
-  animationJump(x,newClass);
+  var btn = this.classList[1]
+
+  let newClass = createPressAnimation(btn)
+  let btnSound = createSoundFileName(btn);
+
+  animationJump(x, newClass, btnSound);
+
+  userInput++;
 });
 
-// functions
-function animationJump(x, newClass) {
+// variables
+var simonSays = [];
+var userInput = 0;
 
+// functions
+
+function loserReset() {
+  simonSays = [];
+  userInput = 0;
+  $(".title").text("You lose");
+  $(".btn-start").text("Try Again");
+}
+
+function compareWithSimon(btn) {
+  if (btn == simonSays[userInput]) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function animationJump(x, newClass, btnSound) {
+  console.log("animation: " + newClass);
+  console.log(x);
   x.toggleClass(newClass);
+
+  var audio = new Audio(btnSound);
+  audio.play();
 
   setTimeout(function() {
     x.toggleClass(newClass);
   }, 250);
-
 }
 
 function getRandomButton() {
   var x = Math.floor(Math.random() * 4) + 1;
-  console.log(x);
   var colour
+  console.log(x);
   switch (x) {
     case 1:
       colour = "btn-blue"
@@ -43,4 +87,39 @@ function getRandomButton() {
       break;
   };
   console.log(colour);
+  return colour;
+}
+
+function addToSimon() {
+  simonSays.push(getRandomButton());
+}
+
+function createPressAnimation(btn) {
+  return (btn + "-press");
+}
+
+function createSoundFileName(btn) {
+  return ("sounds/" + btn + "-sound.mp3");
+}
+
+function playSimonSaysArray() {
+  for (var i = 0; i < simonSays.length; i++) {
+
+
+
+    let x = simonSays[i]
+    let y = "." + x
+
+    let inputClass = createPressAnimation(x)
+    let inputSound = createSoundFileName(x);
+
+    console.log(i + " " + x);
+    console.log(inputClass);
+    console.log(inputSound);
+
+
+    setTimeout(() => {
+      animationJump($(y), inputClass, inputSound);
+    }, 1000 * i);
+  };
 }
